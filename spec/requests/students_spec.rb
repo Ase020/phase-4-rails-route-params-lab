2dd31767fff1076d275264c06c6bd6e524a3520e
felixNyalenda <felixasejunior@gmail.com>
2023-06-19 20:36:36 +0300

    Completed assignment

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe "Students", type: :request do
     # context 'with query params' do
     #   it 'returns students whose first or last name matches the query' do
     #     get '/students?name=kirby'
-
+    #
     #     expect(response.body).to include_json([
     #       { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
     #     ])
